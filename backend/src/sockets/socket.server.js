bcc9246c4677268f6599234aa52266910695609b
binod rai <br163406@gmail.com>
2025-09-16 20:33:46 +0545

    cors with credentials and socketio middleware implemented

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -1,18 +1,41 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
 
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {
-    cors: "http://localhost:5173",
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
+    }
   });
 
-  //use for admin ui
-  // instrument(io, {
-  //   auth: false,
-  //   mode: "development",
-  // });
+  //socket-io middleware
+  io.use( async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers.cookie || "");
 
-  io.on("connection", (socket) => {
+    if(!cookies.token){
+      next(new Error("Authentication Error: no Token provided"));
+    }
+
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET_KEY);
+
+      const user = await userModel.findOne({_id: decoded.id});
+      socket.user = user;
+
+      next();
+
+    } catch (error) {
+      next(new Error("Authentication Error: Invalid token"));
+    }
+  })
+
+
+  io.on("connection", async (socket) => {
     console.log("A user connected:", socket.id);
+    console.log(socket.user.userName)
 
     // listen for chat messages
     socket.on("chatMessage", (msg) => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import AllRoutes from "./mainRoutes/allRoutes"
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000"); // connect to backend
+const socket = io("http://localhost:3000",{
+  withCredentials: true
+});
 
 const App = () => {
+  console.log(socket);
   return (
    <>
      <AllRoutes />
